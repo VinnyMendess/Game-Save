586423af9c1614f80a1551ecf4ec8e0267c69073
VinnyMendess <vinimendesdasilvaa@gmail.com>
2026-08-30 21:44:22 -0300

    Busca na API externa funcionando

--- backend/src/main/java/com/gamesave/demo/biblioteca/Jogos.java
+++ backend/src/main/java/com/gamesave/demo/biblioteca/Jogos.java
@@ -1,4 +1,13 @@
 package com.gamesave.demo.biblioteca;
 
+import java.util.Date;
+
 public class Jogos {
+
+    private String titulo;
+    private Integer horasJogadas;
+    private Date dataConlusao;
+    private String plataforma;
+    private Boolean midia;
+    
 }
---
+++ backend/src/main/java/com/gamesave/demo/biblioteca/JogosController.java
@@ -0,0 +1,10 @@
+package com.gamesave.demo.biblioteca;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class JogosController {
+
+}
